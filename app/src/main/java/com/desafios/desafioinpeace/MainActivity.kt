c9b11648b226d3e1d463436bf4e4cb542ecd34b0
leonardocandado <candadoleo@gmail.com>
2022-10-31 18:27:13 -0400

    Search implementado

--- app/src/main/java/com/desafios/desafioinpeace/MainActivity.kt
+++ app/src/main/java/com/desafios/desafioinpeace/MainActivity.kt
@@ -2,10 +2,11 @@ package com.desafios.desafioinpeace
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.desafios.desafioinpeace.adapter.FriendsAdapter
 import com.desafios.desafioinpeace.databinding.ActivityMainBinding
-import com.desafios.desafioinpeace.databinding.ToolbarBinding
 import com.desafios.desafioinpeace.model.Friend
 import com.desafios.desafioinpeace.model.FriendResponse
 import com.desafios.desafioinpeace.service.FriendApiService
@@ -13,50 +14,62 @@ import com.desafios.desafioinpeace.service.FriendsRepository
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.Locale.filter
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var toolbarBinding: ToolbarBinding
+    lateinit var binding: ActivityMainBinding
+    lateinit var recyclerViewFriends: RecyclerView
+    lateinit var adapter: FriendsAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        val recyclerViewFriends = binding.recyclerViewAmigos
+        recyclerViewFriends = binding.recyclerViewAmigos
         //Define a lista na vertical
         recyclerViewFriends.layoutManager = LinearLayoutManager(this)
+
         getFriendData { friend: List<Friend> ->
-            recyclerViewFriends.adapter = FriendsAdapter(friend)
+            adapter = FriendsAdapter(friend)
+            recyclerViewFriends.adapter = adapter
         }
-
-
+        searchFriend()
     }
 
     private fun getFriendData(callback: (List<Friend>) -> Unit) {
         val apiService = FriendApiService.getInstance().create(FriendsRepository::class.java)
         apiService.getFriendList().enqueue(object : Callback<FriendResponse> {
             override fun onFailure(call: Call<FriendResponse>, t: Throwable) {
-
+                //implementar erro
             }
-
-            override fun onResponse(call: Call<FriendResponse>, response: Response<FriendResponse>) {
+            override fun onResponse(
+                call: Call<FriendResponse>,
+                response: Response<FriendResponse>
+            ) {
                 return callback(response.body()!!.data)
             }
 
         })
     }
 
-    private fun searchFriend (){
-        var search = toolbarBinding.textSearch.toString()
+    private fun searchFriend() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            android.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                adapter.filter.filter(p0)
+                return true
+            }
 
+            override fun onQueryTextChange(p0: String?): Boolean {
+                adapter.filter.filter(p0)
+                return true
+            }
 
-
+        })
     }
-
-
 
 }
--- app/src/main/java/com/desafios/desafioinpeace/adapter/FriendsAdapter.kt
+++ app/src/main/java/com/desafios/desafioinpeace/adapter/FriendsAdapter.kt
@@ -2,16 +2,19 @@ package com.desafios.desafioinpeace.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.desafios.desafioinpeace.databinding.CardAmigosBinding
 import com.desafios.desafioinpeace.model.Friend
 
-class FriendsAdapter (private val items: List<Friend>):
-    RecyclerView.Adapter<FriendsAdapter.FriendsViewHolder>(){
+class FriendsAdapter(private var items: List<Friend>) :
+    RecyclerView.Adapter<FriendsAdapter.FriendsViewHolder>(), Filterable {
 
-    class FriendsViewHolder(val binding: CardAmigosBinding):
-        RecyclerView.ViewHolder(binding.root)
+    private var friendList = items
+
+    class FriendsViewHolder(val binding: CardAmigosBinding) : RecyclerView.ViewHolder(binding.root)
 
     //Cria os itens para a lista (recyclerView)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendsViewHolder {
@@ -25,16 +28,48 @@ class FriendsAdapter (private val items: List<Friend>):
     //Exibir os itens
     override fun onBindViewHolder(holder: FriendsViewHolder, position: Int) {
         val repository = items[position]
-        holder.binding.apply{
+        holder.binding.apply {
             textName.text = (repository.first_name + " " + repository.last_name)
             textEmail.text = repository.email
-            Glide.with(imageFriend)
-                .load(repository.avatar)
-                .into(imageFriend)
-
+            Glide.with(imageFriend).load(repository.avatar).into(imageFriend)
         }
     }
 
     override fun getItemCount(): Int = items.size
+
+    override fun getFilter(): Filter {
+        val filter = object : Filter() {
+            override fun performFiltering(p0: CharSequence?): FilterResults {
+                val filterResult = FilterResults()
+                if (p0 == null || p0.isEmpty()) {
+                    filterResult.values = friendList
+                    filterResult.count = friendList.size
+                } else {
+                    val searchChar = p0.toString()
+                    val filteredResults = ArrayList<Friend>()
+
+                    for (friendModel in items) {
+                        if (friendModel.first_name?.contains(searchChar) == true ||
+                            friendModel.last_name?.contains(searchChar) == true
+                        ) {
+                            filteredResults.add(friendModel)
+                        }
+                    }
+                    filterResult.values = filteredResults
+                    filterResult.count = filteredResults.size
+                }
+
+                return filterResult
+            }
+
+            override fun publishResults(p0: CharSequence?, p1: FilterResults?) {
+                items = p1!!.values as List<Friend>
+                notifyDataSetChanged()
+
+            }
+
+        }
+        return filter
+    }
 
 }
